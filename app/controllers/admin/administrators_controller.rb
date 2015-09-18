7c336f1dfcd3e04dccccd0c4678db1b0bf47d501
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-18 15:57:01 +0100

    Add ability to delete

--- app/controllers/admin/administrators_controller.rb
+++ app/controllers/admin/administrators_controller.rb
@@ -1,15 +1,15 @@
 class Admin::AdministratorsController < ApplicationController
-	#GET /admin/adminsitrators/
+	# GET /admin/adminsitrators/
 	def index
 		@administrators = Administrator.all
 	end
 
-	#Get /admin/administrators/new
+	# Get /admin/administrators/new
 	def new
 		@administrator = Administrator.new
 	end
 
-	#Post /admin/administrators/create
+	# Post /admin/administrators/create
 	def create
 		@administrator = Administrator.new(admin_params)
 
@@ -19,6 +19,21 @@ class Admin::AdministratorsController < ApplicationController
 			render 'new'
 		end
 	end
+
+	# GET /admin/administrator/:id/edit
+	def edit
+		@administrator = Administrator.find(params[:id])
+	end
+
+	# Delete /admin/administrator
+	def destroy
+		@administrator = Administrator.find(params[:id])
+		@admin.delete
+
+		redirect_to admin_administrators_url
+	end
+
+	private
 
 	def admin_params
 		params.require(:administrator).permit(:id, :fullname, :username, :password)
