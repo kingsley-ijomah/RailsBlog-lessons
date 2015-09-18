6245d597fb51723a6b4d5f3ce23daf20c94b5c17
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-18 14:44:17 +0100

    Ability to create new record

--- app/controllers/admin/administrators_controller.rb
+++ app/controllers/admin/administrators_controller.rb
@@ -1,6 +1,26 @@
 class Admin::AdministratorsController < ApplicationController
+	#GET /admin/adminsitrators/
+	def index
+		
+	end
+
 	#Get /admin/administrators/new
 	def new
 		@administrator = Administrator.new
+	end
+
+	#Post /admin/administrators/create
+	def create
+		@administrator = Administrator.new(admin_params)
+
+		if @administrator.save
+			redirect_to admin_administrators_url
+		else
+			render 'new'
+		end
+	end
+
+	def admin_params
+		params.require(:administrator).permit(:id, :fullname, :username, :password)
 	end
 end
