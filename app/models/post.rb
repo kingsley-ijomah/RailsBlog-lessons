db0f5969ba6130aff4451ab16b15161cbfbf34d4
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-19 13:56:53 +0100

    Adding validation to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :administrator
   has_many :post_tags
   has_many :comments
+
+  validates :title, presence: true
+	validates :content, presence: true
+	validates :publish, presence: true
+	validates :administrator_id, presence: true
 end
