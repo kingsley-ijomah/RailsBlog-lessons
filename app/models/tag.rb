5378613fd84d92d4dbeef48767409a92ba2f3481
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-20 20:17:09 +0100

    Adding default status value

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,14 @@
 class Tag < ActiveRecord::Base
+	before_save :default_status
+
 	has_many :post_tags
 	has_many :tags, through: :post_tags
 
 	validates :name, presence: true
+
+	private
+
+	def default_status
+		self.status ||= 0
+	end
 end
