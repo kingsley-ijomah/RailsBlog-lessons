16b156718c4b20fdbfb393ee2902dea2cd3b94e0
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-20 20:33:11 +0100

    Add delete dependent

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :administrator
-  has_many :post_tags
-  has_many :comments
+  has_many :post_tags, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :tags, through: :post_tags
 
   validates :title, presence: true
