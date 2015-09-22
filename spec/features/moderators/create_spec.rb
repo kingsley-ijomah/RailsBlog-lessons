d6e6ff828e6a0d9b71c8b153f7aaf1d18478dc51
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-22 13:51:58 +0100

    Add feature test for moderator create

--- spec/features/moderators/create_spec.rb
+++ spec/features/moderators/create_spec.rb
@@ -1,9 +1,31 @@
 require 'rails_helper'
 
 feature 'Create a moderator' do
-	scenario 'with in-valid fields' do
+	before :each do
 		visit new_admin_moderator_path
-		save_and_open_page
 	end
-	scenario 'with valid fields'
+
+	scenario 'with in-valid fields' do
+		expect {
+			within('#new_moderator') do
+				click_button 'Create Moderator'
+			end
+			expect(page.find('#error_explanation')).to have_content("3 errors prohibited this moderator from being saved")
+			expect(page.find('#error_explanation')).to have_content("Fullname can't be blank")
+			expect(page.find('#error_explanation')).to have_content("Username can't be blank")
+			expect(page.find('#error_explanation')).to have_content("Password can't be blank")
+		}.to change(Moderator, :count).by(0)
+	end
+
+	scenario 'with valid fields' do
+		expect {
+			within('#new_moderator') do
+				fill_in 'moderator_fullname', with: Faker::Name.name
+				fill_in 'moderator_username', with: Faker::Internet.email
+				fill_in 'moderator_password', with: Faker::Internet.password
+
+				click_button 'Create Moderator'
+			end
+		}.to change(Moderator, :count).by(1)
+	end
 end
