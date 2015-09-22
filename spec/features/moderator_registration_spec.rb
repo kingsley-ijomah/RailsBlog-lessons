77fd87b6d937ab3eb2c31f4e1619ee596d2a75d0
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-22 12:14:14 +0100

    Create moderator feature specs

--- spec/features/moderator_registration_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-feature 'Visit registration page' do
-	scenario 'with empty fullname field'
-	scenario 'with empty username field'
-	scenario 'with empty password field'
-	scenario 'with valid fields'
-end
---
+++ spec/features/moderators/create_spec.rb
@@ -0,0 +1,6 @@
+require 'rails_helper'
+
+feature 'Create a moderator' do
+	scenario 'with valid in-valid fields'
+	scenario 'with valid valid fields'
+end
