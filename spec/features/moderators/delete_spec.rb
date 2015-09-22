d860875f4209fe882b66496bc9a935fd044ee860
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-22 18:38:42 +0100

    Write test for deleting moderator

--- spec/features/moderators/delete_spec.rb
+++ spec/features/moderators/delete_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+
+feature 'Delete a moderator' do
+	before :each do
+		create(:moderator)
+	end
+
+	scenario 'successfully delete moderator' do
+		visit admin_moderators_path
+		expect {
+			within '.moderator-row' do
+				click_link 'Delete'
+			end
+		}.to change(Moderator, :count).by(-1)
+	end
+end
---
+++ spec/support/factory_girl.rb
@@ -0,0 +1,6 @@
+# spec/support/factory_girl.rb
+require 'factory_girl_rails'
+
+RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+end
