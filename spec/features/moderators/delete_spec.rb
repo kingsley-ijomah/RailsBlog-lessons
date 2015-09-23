19473d035ec934e541bf8da8c477cbe2d6900010
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-23 07:26:46 +0100

    Update test for moderator delete

--- spec/features/moderators/delete_spec.rb
+++ spec/features/moderators/delete_spec.rb
@@ -1,16 +1,28 @@
 require 'rails_helper'
 
 feature 'Delete a moderator' do
-	before :each do
-		create(:moderator)
-	end
-
 	scenario 'successfully delete moderator' do
-		visit admin_moderators_path
-		expect {
-			within '.moderator-row' do
-				click_link 'Delete'
-			end
-		}.to change(Moderator, :count).by(-1)
+		given_that_i_have_one_moderator
+		and_i_visit_moderators_path
+		when_i_click_on_delete
+		then_i_expect_to_have_no_moderators
 	end
+end
+
+def given_that_i_have_one_moderator
+	create(:moderator)
+end
+
+def and_i_visit_moderators_path
+	visit admin_moderators_path
+end
+
+def when_i_click_on_delete
+	within '.moderator-row' do
+		click_link 'Delete'
+	end
+end
+
+def then_i_expect_to_have_no_moderators
+	expect(Moderator.count).to eq 0
 end
