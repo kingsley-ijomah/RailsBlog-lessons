9764a4e9d24d723803faa508c6c35bee7727e16f
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-24 13:33:09 +0100

    Adding validation test for moderators

--- spec/models/moderator_spec.rb
+++ spec/models/moderator_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Moderator, type: :model do
   describe 'relations' do
   	it { should have_many(:posts).dependent(:destroy) }
   end
+
+  describe 'validations' do
+  	it { should validate_presence_of :fullname }
+  	it { should validate_presence_of :username }
+  	it { should validate_presence_of :password }
+  end
 end
