9d133ba8446110486f424527795a6f238da28c28
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-27 09:41:44 +0100

    Adding a before save method for boolean conversion

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,14 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :visitor
+
+  before_save :status_to_boolean
+
+  def status_to_boolean
+  	if self.status == 'true' 
+  		self.status = true
+  	elsif self.status == 'false'
+  		self.status = false
+  	end
+  end
 end
