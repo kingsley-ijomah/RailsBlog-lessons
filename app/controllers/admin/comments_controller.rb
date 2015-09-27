39b4d64e3062be83788b1ba523e65263c000f7d1
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-27 08:51:46 +0100

    Add ability to fetch comments by status

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,5 +1,12 @@
 class Admin::CommentsController < ApplicationController
   def index
+  	if params[:approved] == 'true'
+  		@comments = Comment.where(status: true)
+  	elsif params[:approved] == 'false'
+  		@comments = Comment.where(status: false)
+  	else
+  		@comments = Comment.all
+  	end
   end
 
   def update
