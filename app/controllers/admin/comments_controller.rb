35352c5874f45f5059eb2162a2dbd6df2ea9494a
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-09-27 09:41:24 +0100

    Ability to update attribute for status

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -5,13 +5,21 @@ class Admin::CommentsController < ApplicationController
   	elsif params[:approved] == 'false'
   		@comments = Comment.where(status: false)
   	else
-  		@comments = Comment.all
+  		@comments = []
   	end
   end
 
   def update
+  	@comment = Comment.find(params[:id])
+  	@comment.update_attribute('status', params[:status])
+
+  	redirect_to :back
   end
 
   def destroy
+  	@comment = Comment.find(params[:id])
+  	@comment.destroy
+
+  	redirect_to admin_comments_url
   end
 end
