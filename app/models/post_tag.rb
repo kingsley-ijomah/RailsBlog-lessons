cb9755bcda55b146648fdacbf415d6301e03661b
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-10-05 10:22:25 +0200

    Adding in_use? method

--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -1,6 +1,4 @@
 class PostTag < ActiveRecord::Base
   belongs_to :post
   belongs_to :tag
-
- # after_save :set_use_status
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,4 +3,8 @@ class Tag < ActiveRecord::Base
 	has_many :tags, through: :post_tags
 
 	validates :name, presence: true
+
+	def in_use?
+		PostTag.exists?(post_id: self.id)
+	end
 end
