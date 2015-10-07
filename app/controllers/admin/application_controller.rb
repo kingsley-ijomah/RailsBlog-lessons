a61f4c75a6783a59194772371999869fd9679312
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-10-07 10:55:26 +0100

    Adding authorize and before_filter method

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,4 +2,15 @@ class Admin::ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :authorize
+
+  def current_moderator
+    @current_moderator ||= Moderator.find(session[:moderator_id]) if session[:moderator_id]
+  end
+  helper_method :current_moderator
+
+  def authorize
+    redirect_to '/login' unless current_moderator
+  end
 end
