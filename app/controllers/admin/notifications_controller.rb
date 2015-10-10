7ad37e62fdd983abb3be5fa1383700e7c15989a3
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-10-10 12:05:27 +0100

    Add delete_all method

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -1,7 +1,21 @@
 class Admin::NotificationsController < Admin::ApplicationController
   def index
+  	@visitor_notifications = Notification.where(:notifiable_type => 'Visitor')
+  	@comment_notifications = Notification.where(:notifiable_type => 'Comment')
   end
 
   def destroy
+  	@notification = Notification.find(params[:id])
+  	@notification.destroy
+
+    flash[:notice] = "Notification dismissed successfully"
+  	redirect_to :back
+  end
+
+  def delete_all
+  	Notification.delete_all
+  	flash[:notice] = "All notifications dismissed successfully"
+
+  	redirect_to :back
   end
 end
