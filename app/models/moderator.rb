5b419f70698031e673562971b3fbd3437a8d44f7
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-10-14 08:23:32 +0100

    Update helper and validation

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -4,6 +4,6 @@ class Moderator < ActiveRecord::Base
 	has_many :posts, dependent: :destroy
 
 	validates :fullname, presence: true
-	validates :username, presence: true, uniqueness: true, format: { with: /@/, message: 'is not a valid email' }
+	validates :username, presence: true, format: { with: /@/, message: 'is not a valid email' }
 	validates :password, presence: true
 end
