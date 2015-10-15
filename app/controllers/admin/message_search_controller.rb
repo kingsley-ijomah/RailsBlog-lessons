bcbf578655531c4320335176c25842952c36a4db
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-10-15 17:02:59 +0100

    Ability to search by both name and content

--- app/controllers/admin/message_search_controller.rb
+++ app/controllers/admin/message_search_controller.rb
@@ -1,7 +1,7 @@
 class Admin::MessageSearchController < Admin::ApplicationController
   def index
   	if params[:search].present? && !params[:search].nil?
-			@messages = Message.where("content like ?", "%#{params[:search]}%").page params[:page]
+			@messages = Message.joins(:visitor).where("content like ? OR fullname like ?", "%#{params[:search]}%", "%#{params[:search]}%").page params[:page]
 		else
 			@messages = []
 		end
