1033823610598ec978cf8a17eaba314d171b44f3
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-10-16 08:46:56 +0100

    Ability to search post by content

--- app/controllers/admin/post_search_controller.rb
+++ app/controllers/admin/post_search_controller.rb
@@ -1,7 +1,8 @@
 class Admin::PostSearchController < Admin::ApplicationController
 	def index
 		if params[:search].present? && !params[:search].nil?
-			@posts = Post.where("title like ?", "%#{params[:search]}%").page params[:page]
+			@posts = Post.where(
+				"title like ? OR content like ?", "%#{params[:search]}%", "%#{params[:search]}%").page params[:page]
 		else
 			@posts = []
 		end
