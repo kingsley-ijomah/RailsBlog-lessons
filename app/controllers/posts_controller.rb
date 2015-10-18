05caf6f27539e02d8f1fc7ea36d3eb22c137a244
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-10-18 08:05:51 +0100

    Set posts per per

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,14 @@
 class PostsController < ApplicationController
   def index
+  	@posts = Post.where(publish: true).order(id: :desc).page(params[:page]).per(Setting.post_per_page)
   end
 
   def show
   end
+
+  private
+
+	def set_post
+		@post = Post.find(params[:id])
+	end
 end
