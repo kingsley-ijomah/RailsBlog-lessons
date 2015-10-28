4d09f75f7f4870792d87650435abbbf929a45805
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-10-28 12:42:46 +0000

    Add edit and update methods

--- app/controllers/admin/moderators_controller.rb
+++ app/controllers/admin/moderators_controller.rb
@@ -2,4 +2,11 @@ class Admin::ModeratorsController < ApplicationController
 	def index
 		@moderators = Moderator.all
 	end
+
+	def edit
+		@moderator = Moderator.find(params[:id])
+	end
+
+	def update
+	end
 end
