31dbd21e165224d319dc1d739e5b162a3fa32830
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-10-30 07:38:15 +0000

    Adding validations

--- app/models/moderator.rb
+++ app/models/moderator.rb
@@ -2,4 +2,8 @@ class Moderator < ActiveRecord::Base
 	has_secure_password
 
 	has_many :posts
+
+	validates :fullname, presence: true
+	validates :username, presence: true, format: {with: /@/, message: 'is not valid'}
+	validates :password, presence: true
 end
