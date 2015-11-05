dda7548851f1e48d94a82122205ef88dcbeec274
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-05 18:35:58 +0000

    Add routing for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get '/login' => 'admin/sessions#new'
+
   namespace :admin do
+    resources :sessions, only: [:new, :create, :destroy]
     resources :moderators, only: [:index, :edit, :update]
   end
 end
