03b7666cfbadb0d86f0d696ed29d49d2f8334a48
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-06 16:46:17 +0000

    Adding destroy

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -5,10 +5,18 @@ class Admin::SessionsController < Admin::ApplicationController
   end
 
   def create
-  	@moderator = Moderator.find_by(username: params[:username]).authenticate(params[:password])
-  	session[:current_moderator_id] = @moderator.id
+  	@moderator = Moderator.find_by(username: params[:username]).try(:authenticate, params[:password])
+  	if @moderator
+  		session[:current_moderator_id] = @moderator.id
+  		redirect_to admin_moderators_url, notice: 'You have successfully signed in'
+  	else
+  		flash[:alert] = 'There was a problem with your username or password'
+  		render :new
+  	end
   end
 
   def destroy
+  	session[:current_moderator_id] = nil
+  	redirect_to '/login', notice: 'You have successfully logged out'
   end
 end
