a0dd9f4836534763dcd288d60a623cb2b4f03115
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-06 16:46:00 +0000

    Adding logout link

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,7 +2,7 @@ class Admin::ApplicationController < ApplicationController
 	before_action :authorize
 	
 	def current_moderator
-		false
+		@moderator ||= Moderator.find(session[:current_moderator_id]) if session[:current_moderator_id]
 	end
 
 	def authorize
