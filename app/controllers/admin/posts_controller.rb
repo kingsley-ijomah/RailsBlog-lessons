f7e385e856968eb1a75f01f569f0f00bd3d6a729
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-07 07:07:34 +0000

    Add posts controller

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,22 @@
+class Admin::PostsController < Admin::ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+end
