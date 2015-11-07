66f1045a867b9c60e29271cf51fd72cfc2bfecf3
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-07 07:05:40 +0000

    Update to contain layout

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,4 +1,10 @@
-class Admin::ApplicationController < ApplicationController
+class Admin::ApplicationController < ActionController::Base
+	# Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  layout 'admin'
+  
 	before_action :authorize
 	
 	def current_moderator
