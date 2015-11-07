e15469008e2aae4df9b8648c8fa98dfd1aba6326
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-07 07:06:51 +0000

    Add posts routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+
   get '/login' => 'admin/sessions#new'
   get '/logout' => 'admin/sessions#destroy'
 
   namespace :admin do
+    resources :posts
     resources :sessions, only: [:new, :create, :destroy]
     resources :moderators, only: [:index, :edit, :update]
   end
