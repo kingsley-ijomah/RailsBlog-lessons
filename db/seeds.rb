819b5d337426754227039dfa7e1c819a3a2b44e1
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-11 19:04:14 +0000

    Updating seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,15 @@ moderator = Moderator.create(
 	fullname: "Kingsley Ijomah",
 	username: "kingsley@example.com",
 	password: "example")
+
+30.times do
+	post = Post.create(
+		title: Faker::Lorem.sentence(20),
+		content: Faker::Lorem.paragraph,
+		publish: true,
+		moderator: moderator)
+
+	tag = Tag.create(name: Faker::Lorem.word)
+
+	post_tag = PostTag.create(post: post, tag: tag)
+end
