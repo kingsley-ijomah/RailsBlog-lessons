04835d382addfaf52b0dc4ca3a6947cada560089
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-14 16:36:03 +0000

    Adding search to index

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -1,6 +1,10 @@
 class Admin::PostsController < Admin::ApplicationController
   def index
-    @posts = Post.all.order(id: :desc).page params[:page]
+    if params[:search].present?
+      @posts = Post.where("title LIKE ? OR content LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%").page params[:page]
+    else
+      @posts = Post.all.order(id: :desc).page params[:page]
+    end
   end
 
   def new
