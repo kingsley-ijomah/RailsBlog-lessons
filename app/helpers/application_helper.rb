7d3c5c315bcdc1f07806f92f220aa1eee3cc30d0
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-15 07:00:04 +0000

    Add helper method for time ago

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
-	def status_converter(status, truthy: nil, falsey: nil)
+	def status_converter(status, truthy: 'Active', falsey: 'Pending')
 		if status
 			truthy
 		else
 			falsey
 		end
+	end
+
+	def time_ago time
+		"#{time_ago_in_words(time)} ago"
 	end
 end
