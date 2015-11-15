9df9340024fa2502df637ba60087dc8976ed2b37
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-15 07:00:39 +0000

    Create matching_title_or_content method

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	has_many :tags, through: :post_tags
   belongs_to :moderator
+
+  def self.matching_title_or_content search
+  	where("title LIKE ? OR content LIKE ?", "%#{search}%", "%#{search}%")
+  end
 end
