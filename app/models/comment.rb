ec4790b8054ee763845cb8950de7d7bc1c51e460
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-16 07:13:38 +0000

    Adding dependent destroy to comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
   belongs_to :visitor
-  has_many :notifications, as: :notifiable
+  has_many :notifications, as: :notifiable, dependent: :destroy
 end
