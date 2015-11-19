f800d948e08f5d6080aa1f87c60095700b0a3860
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-19 14:06:35 +0000

    Add delete method and update method

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -12,15 +12,27 @@ class Admin::TagsController < Admin::ApplicationController
   end
 
   def edit
+    @tag = Tag.find(params[:id])
   end
 
   def update
+    @tag = Tag.find(params[:id])
+    if @tag.update tags_params
+      redirect_to new_admin_tag_url, notice: 'Successfully updated tag'
+    else
+      flash[:alert] = 'There was a problem updating tag'
+      render :edit
+    end
   end
 
   def show
   end
 
   def destroy
+    @tag = Tag.find(params[:id])
+    @tag.destroy
+
+    redirect_to :back, notice: 'Successfully deleted tag'
   end
 
   private
