e097efe2c5e4e76b126c3338f8af0d679ec4f1fd
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-19 00:02:23 +0000

    Adding tags helper

--- app/helpers/admin/tags_helper.rb
+++ app/helpers/admin/tags_helper.rb
@@ -1,2 +1,13 @@
 module Admin::TagsHelper
+	def create_deletable_button tag 
+		if tag.in_use?
+			link_to '#' do
+				content_tag(:button, 'Delete', class: 'disabled')
+			end
+		else
+			link_to admin_tag_path(tag), method: :delete do
+				content_tag(:button, 'Delete')
+			end
+		end
+	end
 end
