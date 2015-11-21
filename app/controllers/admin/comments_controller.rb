b2fb049eed17d4980ba221cdbb90a21e2d7fa4b7
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-21 15:37:39 +0000

    Adding index method

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,10 +1,15 @@
 class Admin::CommentsController < Admin::ApplicationController
   def index
+  	@comments = Comment.where(status: to_bool(params[:status]))
   end
 
   def update
   end
 
   def destroy
+  	@comment = Comment.find(params[:id])
+  	@comment.destroy
+
+  	redirect_to :back, notice: 'Successfully deleted comment'
   end
 end
