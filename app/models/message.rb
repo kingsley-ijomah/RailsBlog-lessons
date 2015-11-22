e430d0071af0b1ab8e0e06001db37f164897ad7a
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-22 11:02:51 +0000

    Adding filter method

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :visitor
+
+  def self.matching_fullname_or_content params
+  	joins(:visitor).where("fullname LIKE ? OR content LIKE ?", "%#{params}%", "%#{params}%")
+  end
 end
