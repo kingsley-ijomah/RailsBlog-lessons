7a77a91657fb73313c5bd28908b9f91749acf350
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-22 01:01:31 +0000

    Adding dependent

--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -1,5 +1,5 @@
 class Visitor < ActiveRecord::Base
-	has_many :notifications, as: :notifiable
-	has_many :comments
-	has_many :messages
+	has_many :notifications, as: :notifiable, dependent: :destroy
+	has_many :comments, dependent: :destroy
+	has_many :messages, dependent: :destroy
 end
