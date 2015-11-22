bc85424fad2e515fb61bdc6c000338949b6fac65
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-22 11:02:40 +0000

    Adding show method

--- app/controllers/admin/messages_controller.rb
+++ app/controllers/admin/messages_controller.rb
@@ -1,9 +1,14 @@
 class Admin::MessagesController < Admin::ApplicationController
   def index
-  	@messages = Message.all.order(id: :desc).page params[:page]
+  	if params[:search].present?
+  		@messages = Message.matching_fullname_or_content(params[:search]).page params[:page]
+  	else
+  		@messages = Message.all.order(id: :desc).page params[:page]
+  	end
   end
 
   def show
+  	@message = Message.find(params[:id])
   end
 
   def update
