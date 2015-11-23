fa97f5b9c02709bda7234045232e36ca7f38cd25
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-23 21:18:45 +0000

    Adding dashboard routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :posts
+    resources :dashboard, only: [:index]
     resources :notifications, only: [:index, :destroy]
     resources :messages, only: [:index, :show, :update, :destroy]
     resources :visitors, only: [:index, :destroy]
