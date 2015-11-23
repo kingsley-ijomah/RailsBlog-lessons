392e822a2f2f6347a8650095ee40685a48d8ac1f
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-23 15:27:57 +0000

    Adding delete

--- app/controllers/admin/notifications_controller.rb
+++ app/controllers/admin/notifications_controller.rb
@@ -1,7 +1,18 @@
 class Admin::NotificationsController < Admin::ApplicationController
   def index
+  	@visitor_notifications = Notification.where(notifiable_type: 'Visitor').order(id: :desc)
+  	@comment_notifications = Notification.where(notifiable_type: 'Comment').order(id: :desc)
   end
 
   def destroy
+  	@notifiable = Notification.find(params[:id])
+  	@notifiable.destroy
+
+  	redirect_to :back, notice: 'Notification was deleted successfully'
+  end
+
+  def delete_all
+  	Notification.delete_all
+  	redirect_to :back, notice: 'All notifications deleted successfully'
   end
 end
