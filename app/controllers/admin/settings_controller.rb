d9b6e90d2c83fbaeb20eafd7fdac60c11b70553b
kingsley ijomah <kingsley.ijomah@gmail.com>
2015-11-24 12:51:13 +0000

    Add ability to create and update setting

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,15 +1,34 @@
 class Admin::SettingsController < Admin::ApplicationController
   def new
-  	@setting = Setting.new
+  	if Setting.any?
+  		redirect_to edit_admin_setting_url(Setting.first)
+  	else
+  		@setting = Setting.new
+  	end
   end
 
   def create
+  	@setting = Setting.new(settings_params)
+  	if @setting.save
+  		redirect_to edit_admin_setting_url(@setting), notice: 'Successfully created setting'
+  	else
+  		flash[:alert] = 'There was a problem creating setting'
+  		render :new
+  	end
   end
 
   def edit
+  	@setting = Setting.find(params[:id])
   end
 
   def update
+  	@setting = Setting.find(params[:id])
+  	if @setting.update(settings_params)
+  		redirect_to edit_admin_setting_url(@setting), notice: 'Successfully updated setting'
+  	else
+  		flash[:alert] = 'There was a problem updating setting'
+  		render :edit 
+  	end
   end
 
   private
